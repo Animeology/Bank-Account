1f4bf68d51b1664958fb6d6745419452c0ca66d0
Animeology <joseph.nguyen19@yahoo.com>
2023-05-25 14:19:45 -0500

    Create Account.cs

---
+++ Account.cs
@@ -0,0 +1,35 @@
+﻿public class Account
+{
+    public void LogInAccount()
+    {
+        Console.WriteLine("Login or Create an account if you don't have an account");
+        Console.WriteLine("1: Login");
+        Console.WriteLine("2: Create");
+
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            Console.Write("Username:");
+            string username = Console.ReadLine()!;
+            Console.Write("Password:");
+            string password = Console.ReadLine()!;
+        }
+        else if (choice == 2)
+        {
+            CreateAccount();
+        }
+    }
+
+    void CreateAccount()
+    {
+        Console.WriteLine("Hello, and Welcome to our Bank. Please input your desired Username and Password");
+
+        Console.Write("Username:");
+        string username = Console.ReadLine()!;
+        Console.Write("Password:");
+        string password = Console.ReadLine()!;
+
+        Console.WriteLine("Thank you for creating your acount");
+    }
+}

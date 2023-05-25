c19072dc370793518f4969ab58b901f730a3fd23
Animeology <joseph.nguyen19@yahoo.com>
2023-05-25 14:20:15 -0500

    Create Bank.cs
    Renamed the main class and create separate classes for the account and transaction

---
+++ Bank.cs
@@ -0,0 +1,12 @@
+﻿public class Bank
+{
+    static void Main(string[] args)
+    {
+        Account account = new Account();
+        Transaction transaction = new Transaction();
+
+        account.LogInAccount();
+        transaction.Menu();
+    }
+
+}

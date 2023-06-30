0a4b5f8428a66e91da36d02b65fd3abc6a1820f4
Animeology <joseph.nguyen19@yahoo.com>
2023-06-30 13:32:48 -0500

    Create BankTests.cs

---
+++ BankTests.cs
@@ -0,0 +1,32 @@
+﻿using BankAccount;
+
+namespace Bank_Account
+{
+    public class BankTests
+    {
+        static void CheckAccount_WrongPassword_ReturnFalse()
+        {
+            string username = "joseph";
+            string password = "12345";
+            string file = username + ".txt";
+
+            Account mockAcc = new Account();
+
+            bool isInvalid = mockAcc.CheckAccount(username, password, file);
+            if(isInvalid)
+            {
+                Console.WriteLine("Test for CheckAccount_WrongPassword_ReturnFalse failed");
+            }
+            else
+            {
+                Console.WriteLine("Test for CheckAccount_WrongPassword_ReturnFalse succeeded");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CheckAccount_WrongPassword_ReturnFalse();
+        }
+
+    }
+}

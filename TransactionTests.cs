6efaf930ad0dc282bc4c455efef83c88ac68fec4
Animeology <joseph.nguyen19@yahoo.com>
2023-07-01 12:58:06 -0500

    Deposit Test

---
+++ TransactionTests.cs
@@ -0,0 +1,56 @@
+﻿using BankAccount;
+
+namespace Bank_Account
+{
+    public class TransactionTests
+    {
+
+        static void Deposit_Test()
+        {
+            // Assign
+            Transaction mockTransaction = new Transaction();
+
+            float mockBalance = 100.0f;
+            float mockAmount = 100.0f;
+            mockTransaction.amount = mockAmount;
+            mockTransaction.balance = mockBalance;
+
+            float expectedBalance = 200.0f;
+
+            // Act
+            string mockFile = CreateMockFile(mockBalance);
+            float actualBalance = mockTransaction.Deposit(mockBalance, mockFile);
+
+            // Assert
+            if(actualBalance == expectedBalance)
+            {
+                Console.WriteLine("Deposit_Test succeeded");
+            }
+            else
+            {
+                Console.WriteLine("Deposit_Test failed");
+            }
+        }
+
+
+
+
+
+        static string CreateMockFile(float balance)
+        {
+            string mockUser = "MockAccount";
+            string mockPass = "MockPassword";
+            string mockFile = mockUser + ".txt";
+
+            File.AppendAllText(mockFile, mockPass + Environment.NewLine);
+            File.AppendAllText(mockFile, balance + Environment.NewLine);
+
+            return mockFile;
+        }
+
+        static void Main(string[] args)
+        {
+            Deposit_Test();
+        }
+    }
+}

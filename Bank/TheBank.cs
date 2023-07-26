03799550ca42699773c768a8c8b00fe352dd4288
Animeology <joseph.nguyen19@yahoo.com>
2023-07-26 18:24:53 -0500

    rename and added both tests in main

---
+++ Bank/TheBank.cs
@@ -0,0 +1,18 @@
+﻿namespace Bank_Account
+{
+    using Tests;
+    using Bank;
+
+    public class TheBank
+    {
+        static void Main(string[] args)
+        {
+            TransactionTests.AllTransactionTests();
+            AccountTests.AllAccountTests();
+
+            Account account = new Account();
+            account.LogInAccount();
+        }
+    }
+
+}
